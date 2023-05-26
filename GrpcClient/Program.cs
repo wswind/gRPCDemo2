25cd3d37bd56fc75302c9034947137e74057be4e
Vincent Wang <ws_dev@163.com>
2023-05-26 15:58:24 +0800

    init

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using ProtoBuf.Grpc.Client;
+using Shared.Contracts;
+
+namespace GrpcGreeterClient;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var httpHandler = new HttpClientHandler();  
+        // Return `true` to allow certificates that are untrusted/invalid  
+        httpHandler.ServerCertificateCustomValidationCallback =   
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;  
+
+        using var channel = GrpcChannel.ForAddress("https://localhost:7057",  new GrpcChannelOptions { HttpHandler = httpHandler });
+        var client = channel.CreateGrpcService<IGreeterService>();
+
+        var reply = await client.SayHelloAsync(
+            new HelloRequest { Name = "GreeterClient" });
+
+        Console.WriteLine($"Greeting: {reply.Message}");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
