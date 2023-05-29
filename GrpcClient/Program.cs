dcbb2516195c375fa4872125b351941e8a2ff5c2
Vincent Wang <ws_dev@163.com>
2023-05-29 14:12:43 +0800

    use client factory

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using Grpc.Net.Client;
-using ProtoBuf.Grpc.Client;
+using Grpc.Net.ClientFactory;
+using ProtoBuf.Grpc.ClientFactory;
+using Microsoft.Extensions.DependencyInjection;
 using Shared.Contracts;
 
 namespace GrpcGreeterClient;
@@ -9,13 +10,41 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        var httpHandler = new HttpClientHandler();  
-        // Return `true` to allow certificates that are untrusted/invalid  
-        httpHandler.ServerCertificateCustomValidationCallback =   
-        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;  
+        string url = "https://localhost:7057";
+        var httpHandler = new HttpClientHandler
+        {
+            // allow certificates
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
 
-        using var channel = GrpcChannel.ForAddress("https://localhost:7057",  new GrpcChannelOptions { HttpHandler = httpHandler });
-        var client = channel.CreateGrpcService<IGreeterService>();
+        //using var channel = GrpcChannel.ForAddress(url,  new GrpcChannelOptions { HttpHandler = httpHandler });
+        //var client = channel.CreateGrpcService<IGreeterService>();
+
+        //var reply = await client.SayHelloAsync(
+        //    new HelloRequest { Name = "GreeterClient" });
+
+        //Console.WriteLine($"Greeting: {reply.Message}");
+        //Console.WriteLine("Press any key to exit...");
+        //Console.ReadKey();
+
+        var services = new ServiceCollection();
+        services.AddCodeFirstGrpcClient<IGreeterService>(o =>
+        {
+            o.Address = new Uri(url);
+            
+            o.ChannelOptionsActions.Add((opt) =>
+            {
+                opt.HttpHandler = httpHandler;
+            });
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var clientFactory = serviceProvider.GetRequiredService<GrpcClientFactory>();
+        var client = clientFactory.CreateClient<IGreeterService>(nameof(IGreeterService));
+
+        var name = client.GetType().FullName;
+        Console.WriteLine("ProtoBuf.Grpc.Internal.Proxies.ClientBase." + name);
 
         var reply = await client.SayHelloAsync(
             new HelloRequest { Name = "GreeterClient" });
